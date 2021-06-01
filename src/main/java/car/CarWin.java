87ffc61a93da3e4455b86e4cd3cf76a1c619bf22
User <paul881125@yahoo.com.tw>
2021-06-01 15:21:28 +0800

    myproject

---
+++ src/main/java/car/CarWin.java
@@ -0,0 +1,16 @@
+package car;
+
+import javax.swing.*;
+
+public class CarWin {
+
+    public static final int WIDTH = 600;
+
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH,600);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
